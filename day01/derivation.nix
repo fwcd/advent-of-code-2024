44aabb421415ed477a5d0d133a403900612ea87f
fwcd <fwcd@fwcd.dev>
2024-11-11 14:30:48 +0100

    Add missing dependency

--- day01/derivation.nix
+++ day01/derivation.nix
@@ -3,6 +3,10 @@
     name = "advent-of-code-2024-day01";
     src = ./src;
 
+    buildInputs = [
+      powershell
+    ];
+
     installPhase = ''
       mkdir -p $out/bin
       cp day01.ps1 $out/bin/day01
