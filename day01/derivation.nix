8d88a3ba3a7bc3b01baa204cfc035212c59fd3ec
fwcd <fwcd@fwcd.dev>
2024-11-11 13:43:53 +0100

    Stub out day 1

---
+++ day01/derivation.nix
@@ -0,0 +1,10 @@
+{ stdenv, powershell }:
+  stdenv.mkDerivation {
+    name = "advent-of-code-2024-day01";
+    src = ./src;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp day01.ps1 $out/bin/day01
+    '';
+  }
