744e2ea672fa880f60f3916f148973fb77833a88
fwcd <fwcd@fwcd.dev>
2024-11-11 14:56:08 +0100

    Add custom launch script for day 1
    There seems to be something funky going on with the shebang patching

--- day01/derivation.nix
+++ day01/derivation.nix
@@ -8,7 +8,12 @@
     ];
 
     installPhase = ''
-      mkdir -p $out/bin
-      cp day01.ps1 $out/bin/day01
+      mkdir -p $out/{bin,share}
+      cp day01.ps1 $out/share
+
+      cat <<EOF > $out/bin/day01
+      exec pwsh "\$(dirname "\$0")/../share/day01.ps1" "$@"
+      EOF
+      chmod +x $out/bin/day01
     '';
   }
