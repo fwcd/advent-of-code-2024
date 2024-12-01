ac542e0fc38b1a7a6c8489fb52cd76575033ba66
fwcd <fwcd@fwcd.dev>
2024-12-01 23:53:49 +0100

    Complete day 1

--- day01/src/day01.ps1
+++ day01/src/day01.ps1
@@ -1,9 +1,17 @@
 #!/usr/bin/env pwsh
 
+function Sum {
+  param ($list)
+  ($list | Measure-Object -Sum).Sum
+}
+
 $input = Get-Content $args[0] | % { ,($_ -split '\s+') }
 $left = $input | % { $_[0] } | Sort-Object
 $right = $input | % { $_[1] } | Sort-Object
-$pairs = (0..($input.Count - 1)) | % { [Math]::Abs($left[$_] - $right[$_]) }
 
-$part1 = ($pairs | Measure-Object -Sum).Sum
+$part1 = Sum $((0..($input.Count - 1)) | % { [Math]::Abs($left[$_] - $right[$_]) })
 Write-Output "Part 1: $part1"
+
+$freqs = $right | Group-Object -AsHashTable
+$part2 = Sum $($left | % { [int]$_ * $freqs[$_].Count })
+Write-Output "Part 2: $part2"
