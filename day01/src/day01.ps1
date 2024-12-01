3c7564a9d3d03bdbc06c08864ebb7a6c05b898f5
fwcd <fwcd@fwcd.dev>
2024-12-01 23:33:28 +0100

    Implement day 1 part 1

--- day01/src/day01.ps1
+++ day01/src/day01.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/env pwsh
 
-$input = Get-Content $args[0]
-Write-Output "Input: $input"
+$input = Get-Content $args[0] | % { ,($_ -split '\s+') }
+$left = $input | % { $_[0] } | Sort-Object
+$right = $input | % { $_[1] } | Sort-Object
+$pairs = (0..($input.Count - 1)) | % { [Math]::Abs($left[$_] - $right[$_]) }
+
+$part1 = ($pairs | Measure-Object -Sum).Sum
+Write-Output "Part 1: $part1"
