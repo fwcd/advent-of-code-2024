c386f9fe436c9cfd6940e485fa5908b8f373a8be
fwcd <fwcd@fwcd.dev>
2024-12-03 17:08:43 +0100

    Read the input file

--- day03/src/day03.pl
+++ day03/src/day03.pl
@@ -1,3 +1,13 @@
 #!/usr/bin/env perl
+use warnings;
+use strict;
+
+my ($inputFile) = @ARGV;
+open(FH, '<', "$inputFile") or die $!;
+
+while (<FH>) {
+  my $line = $_;
+  print "$line";
+}
 
 print "Hello World\n";
