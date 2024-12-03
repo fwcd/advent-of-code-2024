021e5026b3fc87f0afcc1a3b44b4b8c4ccf45523
fwcd <fwcd@fwcd.dev>
2024-12-03 17:31:47 +0100

    Implement day 3 part 2

--- day03/src/day03.pl
+++ day03/src/day03.pl
@@ -11,11 +11,22 @@ if (not defined $inputFile) {
 open(FH, '<', "$inputFile") or die $!;
 
 my $part1 = 0;
+my $part2 = 0;
+my $enabled = 1;
 
 while (<FH>) {
-  while ($_ =~ /mul\((\d+),(\d+)\)/g) {
-    $part1 += $1 * $2;
+  while ($_ =~ /(do(n't)?)\(\)|mul\((\d+),(\d+)\)/g) {
+    if (defined $1) {
+      $enabled = not defined $2;
+    } else {
+      my $product = $3 * $4;
+      $part1 += $product;
+      if ($enabled) {
+        $part2 += $product;
+      }
+    }
   }
 }
 
 print "Part 1: $part1\n";
+print "Part 2: $part2\n";
