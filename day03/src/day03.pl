3b2115afff8e154619eca2890ef5e7fd2b42b3d1
fwcd <fwcd@fwcd.dev>
2024-12-03 17:09:49 +0100

    Require passing input file

--- day03/src/day03.pl
+++ day03/src/day03.pl
@@ -3,6 +3,11 @@ use warnings;
 use strict;
 
 my ($inputFile) = @ARGV;
+
+if (not defined $inputFile) {
+  die "Usage: day03 <path to input> \n";
+}
+
 open(FH, '<', "$inputFile") or die $!;
 
 while (<FH>) {
