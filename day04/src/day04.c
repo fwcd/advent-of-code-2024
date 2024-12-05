8fb11ef0abd2d9e576dd763fc03119917358c824
fwcd <fwcd@fwcd.dev>
2024-12-05 03:03:28 +0100

    Parse the input grid

--- day04/src/day04.c
+++ day04/src/day04.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello world!\n");
+#define GRID_SIZE 140
+
+struct Grid {
+  char data[GRID_SIZE][GRID_SIZE];
+  int width;
+  int height;
+};
+
+struct Grid parse_grid(FILE *input) {
+  struct Grid grid = { .width = 0, .height = 0 };
+  char *line = NULL;
+  size_t length = 0;
+  size_t size = 0;
+
+  while ((length = getline(&line, &size, input)) != -1) {
+    if (length > 0) {
+      memcpy(&grid.data[grid.height], line, length);
+      grid.width = length;
+      grid.height++;
+    }
+  }
+
+  free(line);
+  return grid;
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc <= 1) {
+    fprintf(stderr, "Usage: %s <path to input>\n", argv[0]);
+    return 1;
+  }
+
+  const char *input_path = argv[1];
+  FILE *input = fopen(input_path, "r");
+  if (input == NULL) {
+    fprintf(stderr, "Could not open input\n");
+    return 1;
+  }
+
+  struct Grid grid = parse_grid(input);
+  fclose(input);
+
   return 0;
 }
