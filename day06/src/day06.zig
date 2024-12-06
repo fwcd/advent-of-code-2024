24b243ec799181259e7618b94140846d8e3c893b
fwcd <fwcd@fwcd.dev>
2024-12-06 17:36:49 +0100

    Implement day 6 part 1

--- day06/src/day06.zig
+++ day06/src/day06.zig
@@ -7,7 +7,7 @@ const Map = std.AutoHashMap;
 const String = []u8;
 const Matrix = List(String);
 const Vec2 = struct { x: i32, y: i32 };
-const State = struct { pos: Vec2, dir: Vec2 };
+const Guard = struct { pos: Vec2, dir: Vec2 };
 
 fn Set(comptime T: type) type {
     return Map(T, void);
@@ -21,6 +21,26 @@ fn height(matrix: Matrix) usize {
     return matrix.items.len;
 }
 
+fn vecAdd(a: Vec2, b: Vec2) Vec2 {
+    return .{ .x = a.x + b.x, .y = a.y + b.y };
+}
+
+fn turnRight(dir: Vec2) Vec2 {
+    return .{ .x = -dir.y, .y = dir.x };
+}
+
+fn step(guard: Guard) Guard {
+    return .{ .pos = vecAdd(guard.pos, guard.dir), .dir = guard.dir };
+}
+
+fn inBounds(pos: Vec2, matrix: Matrix) bool {
+    return pos.x >= 0 and pos.x < width(matrix) and pos.y >= 0 and pos.y < height(matrix);
+}
+
+fn get(pos: Vec2, matrix: Matrix) u8 {
+    return matrix.items[@intCast(pos.y)][@intCast(pos.x)];
+}
+
 fn parseDirection(c: u8) ?Vec2 {
     return switch (c) {
         '^' => .{ .x = 0, .y = -1 },
@@ -31,7 +51,7 @@ fn parseDirection(c: u8) ?Vec2 {
     };
 }
 
-fn findStart(matrix: Matrix) State {
+fn findStart(matrix: Matrix) Guard {
     for (0..height(matrix)) |y| {
         for (0..width(matrix)) |x| {
             const dir = parseDirection(matrix.items[y][x]);
@@ -41,6 +61,23 @@ fn findStart(matrix: Matrix) State {
         }
     }
     std.debug.panic("Could not find start", .{});
+}
+
+fn patrol(matrix: Matrix) !Set(Vec2) {
+    var visited = Set(Vec2).init(allocator);
+    var guard = findStart(matrix);
+
+    while (inBounds(guard.pos, matrix)) {
+        try visited.put(guard.pos, {});
+        const next = step(guard);
+        if (inBounds(next.pos, matrix) and get(next.pos, matrix) == '#') {
+            guard.dir = turnRight(guard.dir);
+        } else {
+            guard = next;
+        }
+    }
+
+    return visited;
 }
 
 pub fn main() !u8 {
@@ -73,8 +110,8 @@ pub fn main() !u8 {
         std.log.debug("{s}", .{line});
     }
 
-    const state = findStart(matrix);
-    std.log.debug("{}", .{state});
+    const part1 = (try patrol(matrix)).count();
+    std.log.debug("Part 1: {}", .{part1});
 
     return 0;
 }
