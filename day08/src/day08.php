da8cdc17f3528f54b67ebfb54db26c211549fbf2
fwcd <fwcd@fwcd.dev>
2024-12-08 20:30:40 +0100

    Implement basic extrapolation

--- day08/src/day08.php
+++ day08/src/day08.php
@@ -9,27 +9,71 @@ class Vec2 {
     $this->y = $y;
   }
 
+  static function parse(string $raw): Vec2 {
+    $split = explode(",", $raw);
+    return new Vec2(intval($split[0]), intval($split[1]));
+  }
+
   function __toString(): string {
-    return "($this->x, $this->y)";
+    return "$this->x,$this->y";
+  }
+
+  function add(Vec2 $rhs): Vec2 {
+    return new Vec2($this->x + $rhs->x, $this->y + $rhs->y);
+  }
+
+  function sub(Vec2 $rhs): Vec2 {
+    return new Vec2($this->x - $rhs->x, $this->y - $rhs->y);
   }
 }
 
-function findFrequencyLocations(array $matrix): array {
+function findFrequencyLocations(array $matrix, int $width, int $height): array {
   $freqLocs = [];
 
-  $height = count($matrix);
-  $width = strlen($matrix[0]);
-
   for ($y = 0; $y < $height; $y++) {
-    for ($x = 0; $x < $width; $x++) {
-      $cell = $matrix[$y][$x];
-      if ($cell != '.' && $cell != '') {
-        $freqLocs[$cell] = [...($freqLocs[$cell] ?? []), new Vec2($x, $y)];
+    if (strlen($matrix[$y]) > 0) {
+      for ($x = 0; $x < $width; $x++) {
+        $cell = $matrix[$y][$x];
+        if ($cell != '.') {
+          $freqLocs[$cell] = [...($freqLocs[$cell] ?? []), new Vec2($x, $y)];
+        }
       }
     }
   }
 
   return $freqLocs;
+}
+
+function findAntennas(array $freqLocs, int $width, int $height): array {
+  $antennas = [];
+
+  foreach ($freqLocs as $freq => $locs) {
+    foreach ($locs as $loc) {
+      $antennas["$loc"] = true;
+    }
+  }
+  
+  return $antennas;
+}
+
+function findAntinodes(array $antennas, array $freqLocs, int $width, int $height): array {
+  $antinodes = [];
+
+  foreach ($freqLocs as $freq => $locs) {
+    for ($i = 0; $i < count($locs); $i++) {
+      for ($j = $i + 1; $j < count($locs); $j++) {
+        $diff = $locs[$j]->sub($locs[$i]);
+        echo "{$locs[$i]} to {$locs[$j]} -> $diff" . PHP_EOL;
+        foreach ([$locs[$i]->sub($diff), $locs[$j]->add($diff)] as $antinode) {
+          if (!array_key_exists("$antinode", $antennas)) {
+            $antinodes["$antinode"] = true;
+          }
+        }
+      }
+    }
+  }
+
+  return $antinodes;
 }
 
 if ($argc <= 1) {
@@ -39,7 +83,23 @@ if ($argc <= 1) {
 
 $filePath = $argv[1];
 $raw = file_get_contents($filePath);
-$input = preg_split('/\R/', $raw);
+$matrix = preg_split('/\R/', $raw);
 
-$freqLocs = findFrequencyLocations($input);
-var_dump($freqLocs);
+$height = count($matrix);
+$width = strlen($matrix[0]);
+
+$freqLocs = findFrequencyLocations($matrix, $width, $height);
+$antennas = findAntennas($freqLocs, $width, $height);
+$antinodes = findAntinodes($antennas, $freqLocs, $width, $height);
+
+foreach (array_keys($antinodes) as $rawAntinode) {
+  $antinode = Vec2::parse($rawAntinode);
+  $matrix[$antinode->y][$antinode->x] = '#';
+}
+
+foreach ($matrix as $line) {
+  echo "$line" . PHP_EOL;
+}
+
+$part1 = count($antinodes);
+echo "Part 1: $part1" . PHP_EOL;
