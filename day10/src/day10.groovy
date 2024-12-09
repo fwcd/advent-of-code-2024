8e080b5c7dfc6805678ba5aa7bbebe4f1fbc5893
fwcd <fwcd@fwcd.dev>
2024-12-10 00:37:16 +0100

    Read day 10 input

--- day10/src/day10.groovy
+++ day10/src/day10.groovy
@@ -1,3 +1,10 @@
 #!/usr/bin/env groovy
 
-println "Hello world!"
+if (args.length == 0) {
+  println "Usage: day10 <path to input>"
+  System.exit(1)
+}
+
+String input = new File(args[0]).text
+
+println "Input: $input"
