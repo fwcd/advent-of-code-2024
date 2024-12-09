435dc1f1ca0fb6ddb2726ff101b46ed2d6686ab5
fwcd <fwcd@fwcd.dev>
2024-12-09 06:35:15 +0100

    Implement day 9 part 1

--- day09/src/day09.js
+++ day09/src/day09.js
@@ -5,6 +5,44 @@ if (process.argv.length <= 2) {
   process.exit(1);
 }
 
-const input = fs.readFileSync(process.argv[2], { encoding: 'utf-8' }).trim();
+const rawInput = fs.readFileSync(process.argv[2], { encoding: 'utf-8' }).trim();
+const input = [...rawInput].map(c => parseInt(c));
+const blocks = [...Array(Math.ceil(input.length / 2))]
+  .map((_, i) => ({
+    value: i,
+    count: input[2 * i],
+    free: input[2 * i + 1] ?? 0
+  }));
 
-console.log(input);
+for (let i = 0; i < blocks.length; i++) {
+  const current = blocks[i];
+  const last = blocks[blocks.length - 1];
+  if (current.free > 0) {
+    const moved = Math.min(last.count, current.free);
+    if (moved > 0) {
+      blocks.splice(i + 1, 0, {
+        value: last.value,
+        count: moved,
+        free: current.free - moved,
+      });
+      current.free = 0;
+      last.count -= moved;
+    }
+    if (last.count === 0) {
+      blocks.pop();
+    }
+  }
+}
+
+console.log(blocks);
+
+let part1 = 0;
+let i = 0;
+for (const block of blocks) {
+  for (let j = 0; j < block.count; j++) {
+    part1 += i * block.value;
+    i++;
+  }
+}
+
+console.log(`Part 1: ${part1}`);
