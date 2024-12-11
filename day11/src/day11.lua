6ac6491a4402aa787306b295f6648d3e7c56adc6
fwcd <fwcd@fwcd.dev>
2024-12-11 18:12:05 +0100

    Read the day 11 input

--- day11/src/day11.lua
+++ day11/src/day11.lua
@@ -1,3 +1,19 @@
 #!/usr/bin/env lua
 
-print "Hello"
+if #arg < 1 then
+  print("Usage: day11 <path to input>")
+  os.exit(1)
+end
+
+local stones = {}
+
+for line in io.lines(arg[1]) do
+  for raw in line:gmatch("%d+") do
+    local stone = tonumber(raw)
+    stones[stone] = (stones[stone] or 0) + 1
+  end
+end
+
+for k, v in pairs(stones) do
+  print(tostring(k) .. ' => ' .. tostring(v))
+end
