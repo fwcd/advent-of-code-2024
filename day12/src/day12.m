341922813c92e03949c0e5949896203b4d10b5d8
fwcd <fwcd@fwcd.dev>
2024-12-12 18:02:41 +0100

    Read input

--- day12/src/day12.m
+++ day12/src/day12.m
@@ -1,6 +1,13 @@
 #import <Foundation/Foundation.h>
 
 int main(void) {
-  NSLog(@"Hello world!");
+  NSArray<NSString *> *args = NSProcessInfo.processInfo.arguments;
+  if (args.count < 2) {
+    fprintf(stderr, "usage: %s <path to input>\n", args[0].UTF8String);
+    return 1;
+  }
+
+  NSString *input = [NSString stringWithContentsOfFile:args[1] encoding:NSUTF8StringEncoding error:nil];
+  NSLog(@"Input: %@", input);
   return 0;
 }
