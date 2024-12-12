14d7268092176e641e2531ed80971a4fb3e1f7b4
fwcd <fwcd@fwcd.dev>
2024-12-12 19:08:48 +0100

    Implement day 12 part 2

--- day12/src/day12.m
+++ day12/src/day12.m
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-@interface Vec2 : NSObject
+@interface Vec2 : NSObject <NSCopying>
 
 @property(nonatomic) int x;
 @property(nonatomic) int y;
@@ -13,6 +13,10 @@
   self.x = x;
   self.y = y;
   return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+  return [[Vec2 allocWithZone:zone] initWithX:self.x y:self.y];
 }
 
 - (NSString *)description {
@@ -36,12 +40,17 @@
 @property(nonatomic) int sides;
 @property(nonatomic) int area;
 
-@property(nonatomic, retain) Vec2 *lastBoundaryPos;
-@property(nonatomic, retain) Vec2 *lastBoundaryDir;
+@property(nonatomic, retain) NSMutableDictionary<Vec2 *, NSMutableSet<Vec2 *> *> *boundaryPositionsByDir;
 
 @end
 
 @implementation Region
+
+- (id)initWithPlant:(char)plant {
+  self.plant = plant;
+  self.boundaryPositionsByDir = [[NSMutableDictionary alloc] init];
+  return self;
+}
 
 - (NSString *)description {
   return [NSString stringWithFormat:@"Region[plant=%c, perimeter=%d, sides=%d, area=%d]", self.plant, self.perimeter, self.sides, self.area];
@@ -70,8 +79,41 @@ void dfsRegion(Vec2 *pos, NSArray<NSString *> *matrix, NSMutableSet<Vec2 *> *vis
             dfsRegion(neigh, matrix, visited, region);
           }
         } else {
+          Vec2 *dir = [[Vec2 alloc] initWithX:dx y:dy];
+          if (region.boundaryPositionsByDir[dir] == nil) {
+            region.boundaryPositionsByDir[dir] = [[NSMutableSet alloc] init];
+          }
+          [region.boundaryPositionsByDir[dir] addObject:neigh];
           region.perimeter++;
         }
+      }
+    }
+  }
+}
+
+void dfsSide(Vec2 *pos, NSSet<Vec2 *> *side, NSMutableSet<Vec2 *> *visited) {
+  for (int dy = -1; dy <= 1; dy++) {
+    for (int dx = -1; dx <= 1; dx++) {
+      if (dx == 0 ^ dy == 0) {
+        Vec2 *neigh = [[Vec2 alloc] initWithX:pos.x + dx y:pos.y + dy];
+        if ([side containsObject:neigh] && ![visited containsObject:neigh]) {
+          [visited addObject:neigh];
+          dfsSide(neigh, side, visited);
+        }
+      }
+    }
+  }
+}
+
+void countSides(Region *region) {
+  for (Vec2 *dir in region.boundaryPositionsByDir) {
+    NSMutableSet<Vec2 *> *visited = [[NSMutableSet alloc] init];
+    NSSet<Vec2 *> *side = region.boundaryPositionsByDir[dir];
+    for (Vec2 *pos in side) {
+      if (![visited containsObject:pos]) {
+        [visited addObject:pos];
+        dfsSide(pos, side, visited);
+        region.sides++;
       }
     }
   }
@@ -86,9 +128,9 @@ NSArray<Region *> *findRegions(NSArray<NSString *> *matrix) {
       Vec2 *pos = [[Vec2 alloc] initWithX:x y:y];
       if (![visited containsObject:pos]) {
         [visited addObject:pos];
-        Region *region = [[Region alloc] init];
-        region.plant = plantAt(pos, matrix);
+        Region *region = [[Region alloc] initWithPlant:plantAt(pos, matrix)];
         dfsRegion(pos, matrix, visited, region);
+        countSides(region);
         [regions addObject:region];
       }
     }
@@ -109,7 +151,6 @@ int main(void) {
 
   NSArray<NSString *> *matrix = [input componentsSeparatedByString:@"\n"];
   NSArray<Region *> *regions = findRegions(matrix);
-  NSLog(@"Regions: %@", regions);
 
   int part1 = 0;
   int part2 = 0;
