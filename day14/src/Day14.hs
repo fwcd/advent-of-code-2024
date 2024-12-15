c1e32bf9c18a00918358d60f7d589f88e1d66fe5
fwcd <fwcd@fwcd.dev>
2024-12-15 01:42:20 +0100

    Solve day 14 part 1

--- day14/src/Day14.hs
+++ day14/src/Day14.hs
@@ -3,6 +3,7 @@
 import System.Environment (getArgs)
 import System.IO (readFile')
 import Data.List (sort)
+import qualified Data.Set as S
 import Control.Monad (join)
 
 -- | Splits on the given value.
@@ -13,10 +14,14 @@ split x (y:ys) | x == y    = [] : split x ys
 split _ []                 = [[]]
 
 -- | Finds the period of the given function.
-period :: Eq a => (a -> a) -> a -> [a]
-period f = period' []
-  where period' acc x | x `elem` acc = reverse acc
-                      | otherwise    = period' (x : acc) (f x)
+period :: Ord a => (a -> a) -> a -> [a]
+period f = period' S.empty
+  where period' acc x | x `S.member` acc = []
+                      | otherwise        = x : period' (S.insert x acc) (f x)
+
+-- | The cartesian product with itself.
+cartesianSquare :: [a] -> [(a, a)]
+cartesianSquare xs = [(x, x') | x <- xs, x' <- xs]
 
 -- | Finds the nth element of the list (modulo the list's length) .
 (!!%) :: [a] -> Int -> a
@@ -52,7 +57,7 @@ parseRobot raw = Robot pos vel
   where [pos, vel] = parseVec2 . drop 2 <$> split ' ' raw
 
 boardSize :: Vec2 Int
-boardSize = Vec2 11 7
+boardSize = Vec2 101 103
 
 step :: Robot -> Robot
 step r = Robot ((r.pos .+. r.vel) .%. boardSize) r.vel
@@ -61,9 +66,10 @@ stepN :: Int -> Robot -> Robot
 stepN n = (!!% n) . period step
 
 safetyFactor :: [Robot] -> Int
-safetyFactor rs = length . filter inQuadrant $ (.pos) <$> rs
+safetyFactor rs = foldr1 (*) [length $ filter (\p -> p.x `xop` center.x && p.y `yop` center.y) ps | (xop, yop) <- cartesianSquare [(<), (>)]]
   where center = boardSize ./ 2
         inQuadrant v = v.x /= center.x || v.y /= center.y
+        ps = filter inQuadrant $ (.pos) <$> rs
 
 pretty :: [Robot] -> String
 pretty rs = unlines $ (\y -> join $ (\x -> showCount . length . filter (== Vec2 x y) $ (.pos) <$> rs) <$> [0..boardSize.x]) <$> [0..boardSize.y]
@@ -78,6 +84,5 @@ main = do
       raw <- readFile' path
       let robots = parseRobot <$> lines raw
           robots1 = stepN 100 <$> robots
-      putStrLn $ pretty robots1
       putStrLn $ "Part 1: " ++ show (safetyFactor robots1)
     _ -> putStrLn "Usage: day14 <path to input>"
