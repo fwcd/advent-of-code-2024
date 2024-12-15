a1045945532f9e5c091ef06e5d943e6fea8cb496
fwcd <fwcd@fwcd.dev>
2024-12-15 19:15:29 +0100

    Factor out parsing

--- day15/src/day15.cpp
+++ day15/src/day15.cpp
@@ -3,27 +3,17 @@
 #include <string>
 #include <vector>
 
-struct Board {
-  std::vector<std::string> rows;
-  std::array<int, 2> robot;
-};
-
-int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    std::cerr << "Usage: " << argv[0] << " <path to input>" << std::endl;
-    return 1;
-  }
-
-  Board board;
+struct State {
+  std::vector<std::string> board;
   std::string instructions;
+  std::array<int, 2> robot = {-1, -1};
 
-  {
-    std::ifstream file;
-    file.open(argv[1]);
+  static State parse_from(std::istream &istream) {
+    State state;
 
     bool in_board = true;
     int y = 0;
-    for (std::string line; std::getline(file, line);) {
+    for (std::string line; std::getline(istream, line);) {
       if (in_board) {
         if (line.empty()) {
           in_board = false;
@@ -32,27 +22,44 @@ int main(int argc, char *argv[]) {
           int x = 0;
           for (char cell : line) {
             if (cell == '@') {
-              board.robot = {x, y};
+              state.robot = {x, y};
               row.push_back('.');
             } else {
               row.push_back(cell);
             }
             x++;
           }
-          board.rows.push_back(row);
+          state.board.push_back(row);
         }
       } else {
-        instructions += line;
+        state.instructions += line;
       }
       y++;
     }
+
+    return state;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::cerr << "Usage: " << argv[0] << " <path to input>" << std::endl;
+    return 1;
   }
 
-  std::cout << "Robot: " << board.robot[0] << ", " << board.robot[1] << std::endl;
-  for (const std::string &row : board.rows) {
+  State state;
+
+  {
+    std::ifstream file;
+    file.open(argv[1]);
+    state = State::parse_from(file);
+  }
+
+  std::cout << "Robot: " << state.robot[0] << ", " << state.robot[1] << std::endl;
+  for (const std::string &row : state.board) {
     std::cout << "Row: " << row << std::endl;
   }
-  std::cout << "Instructions: " << instructions << std::endl;
+  std::cout << "Instructions: " << state.instructions << std::endl;
 
   return 0;
 }
