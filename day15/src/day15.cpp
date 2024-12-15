856658be663b65efaf8c8f38c46af3d660ae3d40
fwcd <fwcd@fwcd.dev>
2024-12-15 19:49:53 +0100

    Solve day 15 part 1

--- day15/src/day15.cpp
+++ day15/src/day15.cpp
@@ -1,16 +1,113 @@
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+template <typename T>
+struct Vec2 {
+  T x;
+  T y;
+
+  bool operator<=>(const Vec2<T> &rhs) const = default;
+
+  Vec2<T> operator+(const Vec2<T> &rhs) const {
+    return {x + rhs.x, y + rhs.y};
+  }
+};
+
+enum class Inst : char {
+  Left = '<',
+  Up = '^',
+  Right = '>',
+  Down = 'v',
+};
+
+std::ostream &operator<<(std::ostream &os, Inst inst) {
+  os << char(inst);
+  return os;
+}
+
 struct Board {
   std::vector<std::string> rows;
-  std::array<int, 2> robot = {-1, -1};
+  Vec2<int> robot = {-1, -1};
+
+  int width() const {
+    return rows[0].size();
+  }
+
+  int height() const {
+    return rows.size();
+  }
+
+  bool in_bounds(Vec2<int> pos) const {
+    return pos.x >= 0 && pos.x < width() && pos.y >= 0 && pos.y < height();
+  }
+
+  char operator[](Vec2<int> pos) const {
+    return in_bounds(pos) ? rows[pos.y][pos.x] : '#';
+  }
+
+  char &operator[](Vec2<int> pos) {
+    if (!in_bounds(pos)) throw std::runtime_error("Not in bounds");
+    return rows[pos.y][pos.x];
+  }
+
+  void perform(Inst inst) {
+    Vec2<int> dir;
+    switch (inst) {
+    case Inst::Left: dir = {-1, 0}; break;
+    case Inst::Up: dir = {0, -1}; break;
+    case Inst::Right: dir = {1, 0}; break;
+    case Inst::Down: dir = {0, 1}; break;
+    }
+
+    Vec2<int> next = robot + dir;
+    Vec2<int> end = next;
+    while ((*this)[end] == 'O') {
+      end = end + dir;
+    }
+
+    if ((*this)[end] != '#') {
+      robot = next;
+      if ((*this)[next] == 'O' && (*this)[end] == '.') {
+        std::swap((*this)[next], (*this)[end]);
+      }
+    }
+  }
+
+  int sum_box_coords() const {
+    int sum = 0;
+
+    for (int y = 0; y < height(); y++) {
+      for (int x = 0; x < width(); x++) {
+        if ((*this)[{x, y}] == 'O') {
+          sum += 100 * y + x;
+        }
+      }
+    }
+
+    return sum;
+  }
 };
+
+std::ostream &operator<<(std::ostream &os, const Board &board) {
+  for (int y = 0; y < board.height(); y++) {
+    for (int x = 0; x < board.width(); x++) {
+      if (board.robot == Vec2<int>({x, y})) {
+        os << '@';
+      } else {
+        os << board[{x, y}];
+      }
+    }
+    os << std::endl;
+  }
+  return os;
+}
 
 struct State {
   Board board;
-  std::string instructions;
+  std::vector<Inst> insts;
 
   static State parse_from(std::istream &istream) {
     State state;
@@ -36,12 +133,20 @@ struct State {
           state.board.rows.push_back(row);
         }
       } else {
-        state.instructions += line;
+        for (char raw_inst : line) {
+          state.insts.push_back(Inst(raw_inst));
+        }
       }
       y++;
     }
 
     return state;
+  }
+
+  void run() {
+    for (Inst inst : insts) {
+      board.perform(inst);
+    }
   }
 };
 
@@ -59,11 +164,13 @@ int main(int argc, char *argv[]) {
     state = State::parse_from(file);
   }
 
-  std::cout << "Robot: " << state.board.robot[0] << ", " << state.board.robot[1] << std::endl;
-  for (const std::string &row : state.board.rows) {
-    std::cout << "Row: " << row << std::endl;
-  }
-  std::cout << "Instructions: " << state.instructions << std::endl;
+  // std::cout << state.board;
+  // for (Inst inst : state.insts) {
+  //   state.board.perform(inst);
+  //   std::cout << "After " << inst << ":" << std::endl << state.board;
+  // }
+  state.run();
+  std::cout << "Part 1: " << state.board.sum_box_coords() << std::endl;
 
   return 0;
 }
