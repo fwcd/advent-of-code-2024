8b7f51dad4a52fedf9dc958adbe637291d8f1da8
fwcd <fwcd@fwcd.dev>
2024-12-16 06:08:39 +0100

    Read day 16 lines

--- day16/src/day16.kts
+++ day16/src/day16.kts
@@ -1,3 +1,11 @@
 #!/usr/bin/env kotlin
 
-println("Hello world!")
+import java.io.File
+import kotlin.system.exitProcess
+
+if (args.isEmpty()) {
+  println("Usage: day16 <path to input>")
+  exitProcess(1)
+}
+
+val lines = File(args[0]).readLines()
