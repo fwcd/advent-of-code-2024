457c0fee266d6d09acc72875e4575d943fdedc3e
fwcd <fwcd@fwcd.dev>
2024-12-17 18:26:49 +0100

    Parse program and registers

--- day17/src/day17.cs
+++ day17/src/day17.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 
 if (args.Length == 0)
@@ -7,9 +9,11 @@ if (args.Length == 0)
   return 1;
 }
 
-string input = File.ReadAllText(args[0]);
+string[] rawChunks = File.ReadAllText(args[0]).Trim().Split("\n\n");
+List<int> registers = rawChunks[0].Split("\n").Select(l => int.Parse(l.Split(": ")[1])).ToList();
+List<int> program = rawChunks[1].Split(" ")[1].Split(",").Select(int.Parse).ToList();
 // TODO
 
-Console.WriteLine($"Input: {input}");
+Console.WriteLine($"Program: {string.Join(",", program)}, registers: {string.Join(",", registers)}");
 
 return 0;
