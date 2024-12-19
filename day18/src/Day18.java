fb6bc85c87da9f0f7e8d8c679e6a32999308d3f6
fwcd <fwcd@fwcd.dev>
2024-12-19 02:58:38 +0100

    Solve day 18 part 2

--- day18/src/Day18.java
+++ day18/src/Day18.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,7 +12,10 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day18 {
-  private static record Vec2(int x, int y) {}
+  private static record Vec2(int x, int y) {
+    @Override
+    public final String toString() { return x + "," + y; }
+  }
 
   private static record Node(Vec2 pos, List<Vec2> path, int total) {}
 
@@ -28,7 +32,7 @@ public class Day18 {
 
     public final Vec2 getEnd() { return new Vec2(getWidth() - 1, getHeight() - 1); }
 
-    public final int findShortestPath() {
+    public final OptionalInt findShortestPath() {
       // Run-of-the-mill Dijkstra implementation
 
       Vec2 start = getStart();
@@ -43,7 +47,7 @@ public class Day18 {
       while (!queue.isEmpty()) {
         Node node = queue.poll();
         if (node.pos.equals(end)) {
-          return node.total;
+          return OptionalInt.of(node.total);
         }
         for (int dy = -1; dy <= 1; dy++) {
           for (int dx = -1; dx <= 1; dx++) {
@@ -58,7 +62,7 @@ public class Day18 {
         }
       }
 
-      throw new RuntimeException("No path found");
+      return OptionalInt.empty();
     }
 
     @Override
@@ -67,6 +71,17 @@ public class Day18 {
         .map(row -> row.stream().map(c -> c.toString()).collect(Collectors.joining()))
         .collect(Collectors.joining("\n"));
     }
+  }
+
+  private static OptionalInt solve(Set<Vec2> positions, int size) {
+    Board board = new Board(
+      IntStream.range(0, size).mapToObj(y ->
+        IntStream.range(0, size).mapToObj(x ->
+          positions.contains(new Vec2(x, y)) ? '#' : '.'
+        ).collect(Collectors.toCollection(ArrayList::new))
+      ).collect(Collectors.toCollection(ArrayList::new))
+    );
+    return board.findShortestPath();
   }
 
   public static void main(String[] args) throws IOException {
@@ -78,21 +93,21 @@ public class Day18 {
     int prefix = 1024;
     int size = 71;
 
-    Set<Vec2> positions = Files.readAllLines(Paths.get(args[0]))
+    List<Vec2> positions = Files.readAllLines(Paths.get(args[0]))
       .stream()
       .map(l -> l.split(","))
       .map(s -> new Vec2(Integer.parseInt(s[0]), Integer.parseInt(s[1])))
-      .limit(prefix)
-      .collect(Collectors.toCollection(HashSet::new));
+      .toList();
     
-    Board board = new Board(
-      IntStream.range(0, size).mapToObj(y ->
-        IntStream.range(0, size).mapToObj(x ->
-          positions.contains(new Vec2(x, y)) ? '#' : '.'
-        ).collect(Collectors.toCollection(ArrayList::new))
-      ).collect(Collectors.toCollection(ArrayList::new))
-    );
-
-    System.out.println("Part 1: " + board.findShortestPath());
+    int part1 = solve(positions.stream().limit(prefix).collect(Collectors.toSet()), size).orElseThrow();
+    System.out.println("Part 1: " + part1);
+    
+    String part2 = IntStream.iterate(0, i -> i + 1)
+      .filter(i -> solve(positions.stream().limit(i + 1).collect(Collectors.toSet()), size).isEmpty())
+      .mapToObj(positions::get)
+      .findFirst()
+      .orElseThrow()
+      .toString();
+    System.out.println("Part 2: " + part2);
   }
 }
