7c7712a86f325d40a6306fd7020495b97d685959
fwcd <fwcd@fwcd.dev>
2024-12-23 22:20:13 +0100

    Implement the 0-robots problem as a search on the numpad

--- day21/src/day21.scala
+++ day21/src/day21.scala
@@ -110,10 +110,44 @@ def makePads(robots: Int): List[Pad] = List.fill(robots)(Pad(PadType.Dir)) :+ Pa
 
 def makeState(robots: Int) = State(makePads(robots))
 
-def solve(robots: Int, goals: List[String]): Int =
-  val state = makeState(robots)
+def cost(robots: Int, pos: Vec2, action: Char): Int = 1
+
+def shortestProgramLength(robots: Int, goal: String): Int =
+  case class Node(pos: Vec2, output: String = "", total: Int = 0) extends Ordered[Node] {
+    def compare(that: Node): Int = that.total compare total // Intentionally reversed for min-heap
+  }
+
+  // Your run-of-the-mill Dijkstra implementation (this time on the numpad)
+
+  val queue = mutable.PriorityQueue[Node]()
+  val visited = mutable.HashSet[(Vec2, String)]()
+
+  val startPos = PadType.Num.locate('A')
+  val start = Node(startPos)
+  queue.enqueue(start)
+  visited.add((start.pos, start.output))
+
+  while !queue.isEmpty do
+    val node = queue.dequeue()
+    if node.output == goal then
+      return node.total
+
+    if node.output.length < goal.length then
+      for
+        action <- ACTIONS
+      do
+        val newPos = node.pos + DIRECTIONS.get(action).getOrElse(Vec2(0, 0))
+        if PAD_LAYOUTS(PadType.Num).contains(newPos) then
+          val newOutput = if action == 'A' then node.output.appended(PAD_LAYOUTS(PadType.Num)(node.pos)) else node.output
+          if !visited.contains((newPos, newOutput)) then
+            visited.add((newPos, newOutput))
+            queue.enqueue(Node(newPos, newOutput, node.total + cost(robots, node.pos, action)))
+
+  throw new RuntimeException("No shortest program found")
+
+def solve(robots: Int, goals: List[String], func: (Int, String) => Int): Int =
   goals.map { goal =>
-    val shortest = shortestProgram(state, goal).length
+    val shortest = func(robots, goal)
     shortest * goal.dropRight(1).toInt
   }.sum
 
@@ -122,5 +156,8 @@ def solve(robots: Int, goals: List[String]): Int =
   // println(s"Part 1: ${solve(2, goals)}")
   // println(s"Part 2: ${solve(25, goals)}")
 
-  for c <- ('0' to '5') do
-    println(s"$c -> ${(0 to 3).map { i => shortestProgram(makeState(i), s"$c").length }}")
+  for i <- (0 to 3) do
+    println(s"${solve(i, goals, { (r, g) => shortestProgram(makeState(r), g).length })} vs ${solve(i, goals, shortestProgramLength)}")
+
+  // for c <- ('0' to '5') do
+  //   println(s"$c -> ${(0 to 3).map { i => shortestProgram(makeState(i), s"$c").length }}")
