7c87cd100ae5e984863a4cf4a41c4edbbe3fc531
fwcd <fwcd@fwcd.dev>
2024-12-24 06:11:09 +0100

    Use the Dart JIT instead of the AOT compiler
    The JIT might be marginally faster

--- day22/derivation.nix
+++ day22/derivation.nix
@@ -3,24 +3,17 @@
     name = "advent-of-code-2024-day22";
     src = ./src;
 
-    nativeBuildInputs = [
+    buildInputs = [
       dart
     ];
 
-    buildPhase = ''
-      mkdir -p {tmpbin,out}
-
-      # Ad-hoc signing is required on arm64 macOS, this fixes it (not sure why
-      # the dart derivation doesn't provide it)
-      ${if stdenv.isDarwin then ''
-        ln -s /usr/bin/codesign tmpbin/codesign
-      '' else ""}
-
-      PATH="./tmpbin:$PATH" dart compile exe day22.dart -o out/day22
-    '';
-
     installPhase = ''
-      mkdir -p $out/bin
-      cp out/day22 $out/bin
+      mkdir -p $out/{bin,share}
+      cp day22.dart $out/share
+
+      cat <<EOF > $out/bin/day22
+      exec "${dart.outPath}/bin/dart" run "\$(dirname "\$0")/../share/day22.dart" "\$@"
+      EOF
+      chmod +x $out/bin/day22
     '';
   }
