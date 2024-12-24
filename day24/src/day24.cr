fab85b4fe2f12f2f2932cc51a4f9d3c97fe557d1
fwcd <fwcd@fwcd.dev>
2024-12-24 20:16:31 +0100

    Try swapping some outputs around

--- day24/src/day24.cr
+++ day24/src/day24.cr
@@ -1,11 +1,15 @@
 BITS = 1
 
-alias Vars = Array(Array(String))
+alias Vars = Array(Tuple(String, Int64))
 alias Circuit = Array(Tuple(Array(String), String))
 
 def parse_input(raw : String) : Tuple(Vars, Circuit)
   raw_vars, raw_circuit = raw.split("\n\n").map { |r| r.lines }
-  vars = raw_vars.map { |r| r.split(": ") }
+  vars = raw_vars.map do |r|
+    var, raw_value = r.split(": ")
+    value = raw_value.to_i64
+    {var, value}
+  end
   circuit = raw_circuit.map do |r|
     raw_expr, res = r.split(" -> ")
     expr = raw_expr.split(' ')
@@ -60,6 +64,33 @@ def swap(i : Int, j : Int, circuit : Circuit) : Circuit
   new_circuit
 end
 
+def find_best_swap(vars : Vars, circuit : Circuit) : Tuple(Int32, Int32)
+  output_vars = solve(translate_to_z3(vars, circuit))
+  x = extract_int("x", output_vars)
+  y = extract_int("y", output_vars)
+
+  smallest = 1000000
+  smallest_i = -1
+  smallest_j = -1
+  (0...circuit.size)
+    # Baed on manual analysis of the GraphViz plot, the swaps seem to be relatively local
+    .flat_map { |i| ({i - 4, 0}.max..{i + 4, circuit.size - 1}.min).select { |j| i != j }.map { |j| {i, j} } }
+    .each do |pair|
+      i, j = pair
+      output_vars = solve(translate_to_z3(vars, swap(i, j, circuit)))
+      z = extract_int("z", output_vars)
+      d = hamming_dist(x + y, z)
+      if d < smallest
+        puts "#{i}, #{j} -> #{d} (#{x.to_s(16)} vs #{y.to_s(16)} -> #{z.to_s(16)})"
+        smallest = d
+        smallest_i = i
+        smallest_j = j
+      end
+      d
+    end
+  {smallest_i, smallest_j}
+end
+
 def hamming_dist(n : Int64, m : Int64) : Int64
   (n ^ m).popcount
 end
@@ -92,18 +123,18 @@ else
   part1 = extract_int("z", solve(translate_to_z3(vars, circuit)))
   puts "Part 1: #{part1}"
 
-  circuit = swap(3, 4, circuit)
-  output_vars = solve(translate_to_z3(vars, circuit))
-  x = extract_int("x", output_vars)
-  y = extract_int("y", output_vars)
-  z = extract_int("z", output_vars)
-  puts "z: #{z.to_s(16)}, x + y: #{(x + y).to_s(16)}, hdist: #{hamming_dist(x + y, z)}"
+  swapped = [] of String
+  4.times do |n|
+    i, j = find_best_swap(vars, circuit)
+    swapped << circuit[i][1]
+    swapped << circuit[j][1]
+    circuit = swap(i, j, circuit)
 
-  # (0...vars.size).each do |i|
-  #   output_vars = solve(translate_to_z3(vars, [*circuit[..(i - 1)], *circuit[(i + 1)..]]))
-  #   x = extract_int("x", output_vars)
-  #   y = extract_int("y", output_vars)
-  #   z = extract_int("z", output_vars)
-  #   puts "i = #{i} -> x: #{x}, y: #{y}, z: #{z}, x + y: #{x + y}"
-  # end
+    puts "#{i}, #{j}"
+
+    # Change up the circuit's inputs a bit
+    vars[n] = {vars[n][0], 1_i64 - vars[n][1]}
+  end
+  swapped.sort!
+  puts "Part 2: #{swapped.join(",")}"
 end
