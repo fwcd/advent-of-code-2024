d2efd89ae947e0b3c2035abd8d98cbae7cd069cd
fwcd <fwcd@fwcd.dev>
2024-12-24 05:03:58 +0100

    Read day 23 input

--- day23/src/day23.go
+++ day23/src/day23.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	args := os.Args
+	if len(args) <= 1 {
+		fmt.Println("usage:", args[0], "<path to input>")
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	input := strings.Split(string(data), "\n")
+	fmt.Println(input)
 }
